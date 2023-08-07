f4e8d22b9f1108d47108a0e9c2a1c767e766565f
ThuyNDsmtp <smtpxmht@gmail.com>
2023-08-07 16:45:54 +0700

    ThuyND.txt

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,16 +1,17 @@
-// See https://aka.ms/new-console-template for more information
+//Chuyển đổi kiểu dữ liệu
+string str = "123";
 
-int a = 1;
-long b = 2;
-float c = 3;
-double d = 4;
-decimal e = 5;
-bool f = true;
-char g = 'g';
-string h = "Đây là một chuỗi.";
-//Kiểu thứ tự
-System.Console.WriteLine("Khai báo dữ liệu: int {0}, long {1}, float {2}, double {3}, decimal {4}, bool {5}, character {6}, string {7}",a,b,c,d,e,f,g,h);
-//Kiểu biến
-System.Console.WriteLine($"Khai báo dữ liệu: int {a}, long {b}, float {c}, double {d}, decimal {e}, bool {f}, character {g}, string {h}",a,b,c,d,e,f,g,h);
+int i = int.Parse(str);
+System.Console.WriteLine("1. {0}",i);
 
+int.TryParse(str,out i);
+System.Console.WriteLine("2. {0}",i);
+System.Console.WriteLine("2.1 {0}",int.TryParse(str,out i));
+System.Console.WriteLine("2.2 {0}",int.TryParse("la so 123",out i));
 
+i=Convert.ToInt32(str);
+System.Console.WriteLine("3. {0}",i);
+
+float f = 123;
+
+System.Console.WriteLine("4. {0}",(int)f);
