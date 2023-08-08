53355d6976587eda4086d883f23f419313d11f52
ThuyNDsmtp <smtpxmht@gmail.com>
2023-08-08 16:59:35 +0700

    Buoi4

--- NewApp/Models/Student.cs
+++ NewApp/Models/Student.cs
@@ -8,12 +8,12 @@ namespace NewApp.Models
     public class Student:Person
     {
         public string StudentCode { get; set; }
-        public void EnterData(){
+        public new void EnterData(){
             System.Console.Write("Nhập mã sinh viên: ");
             StudentCode = Console.ReadLine();
             base.EnterData();
         }
-        public void Display(){
+        public new void Display(){
             System.Console.WriteLine("Mã SV {0}, họ và tên SV {1}, địa chỉ SV {2}, tuổi SV {3}.",StudentCode,base.FullName,base.Address,base.Age);
         }
     }
