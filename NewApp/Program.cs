e5da09636c3b6e4c2ddf5fa343d962e47a810f5e
ThuyNDsmtp <smtpxmht@gmail.com>
2023-08-08 13:43:10 +0700

    Buoi4

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -4,16 +4,12 @@ namespace NewApp;
 class Program{
     public static void Main(){
         System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+        System.Console.InputEncoding = System.Text.Encoding.Unicode;
 
         Person p = new();
         p.Display();
 
         p.EnterData();
-        p.Display();
-
-        p.FullName ="Nguyễn Đức Thủy";
-        p.Address = "Hải Dương";
-        p.Age = 54;
         p.Display();
 
         Student s = new Student("Nguyễn Đức Thủy","Hải Dương",1970);
